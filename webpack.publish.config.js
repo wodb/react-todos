4ea1689cca0e4448be2306e93736b393ae9cad59
machuang <632190797@qq.com>
2016-11-24 15:10:24 +0800

    完成功能，还未进行优化

--- webpack.publish.config.js
+++ webpack.publish.config.js
@@ -1,7 +1,12 @@
 var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    entry:path.resolve(__dirname,'src/app.js'),
+    //entry:path.resolve(__dirname,'src/app.js'),
+    entry:{
+      app:path.resolve(__dirname,'src/app.js'),
+        vendors:['react','react-dom']
+    },
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'bundle.js'
@@ -9,13 +14,20 @@ module.exports = {
     module:{
         loaders:[
             {
-                text:/\.jsx?$/,
+                test:/\.jsx?$/,
                 exclude:/(node_modules|bower_components)/,
                 loader:'babel',
                 query:{
                     presets:['es2015','react']
                 }
+            },{
+                test: /\.css$/, // Only .css files
+                exclude: /(node_modules|bower_components)/,
+                loader: 'style!css' // Run both loaders
             }
         ]
-    }
+    },
+    plugins:[
+        new webpack.optimize.CommonsChunkPlugin('vendors','vendors.js')
+    ]
 }
